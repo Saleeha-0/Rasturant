5e32c48d56fc1f14c93c82f6eb58981c5d50ddcc
Saleeha-0 <saleehaakram2@gmail.com>
2024-12-15 03:29:41 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/home'
+import Cart from './pages/Cart/cart'
+import PlaceOrder from './pages/PlaceOrder/placeOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={< Home />} />
+        <Route path='/cart' element={< Cart />} />
+        <Route path='/order' element={ <PlaceOrder/> } />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'> 
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam beatae architecto ex quibusdam commodi. Est repellat veniam tenetur sit, eius voluptas dolore ipsam quibusdam vel quidem sed ab facilis corrupti.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
