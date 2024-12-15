85a14f85e4978bf27fdf0a6c4b741b2e33eee345
Saleeha-0 <saleehaakram2@gmail.com>
2024-12-16 01:44:43 +0800

    Updated features

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
 
 const home = () => {
 
@@ -12,6 +13,7 @@ const home = () => {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
